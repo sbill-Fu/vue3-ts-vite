7a1f97dcb0dd851f12f00cb9a76aaf51be3090c9
sbill <1422347728@qq.com>
2022-08-06 15:09:18 +0800

    feat: 初始化仓库

---
+++ src/main.js
@@ -0,0 +1,35 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css';
+import request from './utils/request'
+import storage from './utils/storage'
+import api from './api'
+import store from './store'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
+console.log("环境变量=>", import.meta.env)
+const app = createApp(App);
+app.directive('has', {
+    beforeMount: function (el, binding) {
+        let actionList = storage.getItem('actionList');
+        let value = binding.value;
+        let hasPermission = actionList.includes(value)
+        if (!hasPermission) {
+            el.style = 'display:none';
+            setTimeout(() => {
+                el.parentNode.removeChild(el);
+            }, 0)
+        }
+    }
+})
+app.config.globalProperties.$request = request;
+app.config.globalProperties.$api = api;
+app.config.globalProperties.$storage = storage;
+
+// 注册全局图标
+for (let [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+  }
+app.use(router).use(store).use(ElementPlus).mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="login-wrapper">
+    <div class="modal">
+      <el-form ref="userFormRef" :model="user" status-icon :rules="rules">
+        <div class="title">火星</div>
+        <el-form-item prop="userName">
+          <el-input
+            type="text"
+            prefix-icon="el-icon-user"
+            v-model="user.userName"
+          />
+        </el-form-item>
+        <el-form-item prop="userPwd">
+          <el-input
+            type="password"
+            prefix-icon="el-icon-view"
+            v-model="user.userPwd"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="btn-login" @click="login(userFormRef)"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from '@vue/reactivity';
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+import api from '@/api'
+let user = reactive({
+  userName: "admin",
+  userPwd: "123456",
+})
+let rules = {
+  userName: [
+    {
+      required: true,
+      message: "请输入用户名",
+      trigger: "blur",
+    },
+  ],
+  userPwd: [
+    {
+      required: true,
+      message: "请输入密码",
+      trigger: "blur",
+    },
+  ],
+}
+const userFormRef = ref()
+const store = useStore();
+const router = useRouter();
+function login(formRef) {
+  formRef.validate((valid) => {
+    if (valid) {
+      api.login(user).then(async (res) => {
+        store.commit("saveUserInfo", res);
+        router.push("/welcome");
+      });
+    } else {
+      return false;
+    }
+  });
+}
+function goHome() {
+  router.push("/welcome");
+}
+</script>
+
+<style lang="scss">
+.login-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9fcff;
+  width: 100vw;
+  height: 100vh;
+  .modal {
+    width: 500px;
+    padding: 50px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0px 0px 10px 3px #c7c9cb4d;
+    .title {
+      font-size: 50px;
+      line-height: 1.5;
+      text-align: center;
+      margin-bottom: 30px;
+    }
+    .btn-login {
+      width: 100%;
+    }
+  }
+}
+</style>
